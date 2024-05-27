9d7e7ba1de9555c8b381de304432b4a07776dfc1
wlyle473 <wlyle473@gmail.com>
2024-05-27 15:16:47 +0100

    Added cleaning of empty directories: - Also renames the model directories so that new models have the correct directory number.

--- src/model_manager.py
+++ src/model_manager.py
@@ -10,6 +10,9 @@ class ModelManager:
         self.device = device
 
     def initialise_model(self):
+
+        self.clean_empty_directories()
+
         config = deepcopy(CONFIG)
 
         if not os.path.exists("model_checkpoints"):
@@ -36,3 +39,29 @@ class ModelManager:
         optimiser.load_state_dict(config["model"]["optimiser_state_dict"])
 
         return config, model, optimiser, hyperparams, checkpoint_directory
+    
+    def clean_empty_directories(self):
+        
+        models_dir = "model_checkpoints"
+        for model in os.listdir(models_dir):
+            if len(os.listdir(f"{models_dir}/{model}")) == 0:
+                os.rmdir(f"{models_dir}/{model}")
+
+        self.rename_files()
+
+    def rename_files(self):
+        
+        models_dir = "model_checkpoints"
+        model_dir_names = sorted(os.listdir(models_dir), key=lambda x:int(x), reverse=True) # Start from the end
+        new_dir_num = len(model_dir_names) - 1 # Zero-indexed
+        
+        # Rename directories to avoid clashes with renaming
+        for old_model_num in model_dir_names:
+            os.rename(src=f"{models_dir}/{old_model_num}", dst=f"{models_dir}/_{old_model_num}")
+
+        model_dir_names = sorted(os.listdir(models_dir), key=lambda x:int(x.strip("_")), reverse=True) # Start from the end again
+        for old_model_num in model_dir_names:
+            # Rename directory
+            os.rename(src=f"{models_dir}/{old_model_num}", dst=f"{models_dir}/{new_dir_num}")
+            new_dir_num -= 1
+        
